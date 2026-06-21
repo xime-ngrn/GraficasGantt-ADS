3cba38f6bdc0bac17b03809c7e374cee0486442d
xime-ngrn <moreno.nogueron.ximena@gmail.com>
2026-06-21 01:11:55 -0600

    Cambio a la base de datos para la generación de 3 Proyectos iniciales por defecto

--- BaseDatos/diagramagantt_backup.sql
+++ BaseDatos/diagramagantt_backup.sql
@@ -1,61 +1,8 @@
--- MySQL dump 10.13  Distrib 8.3.0, for Win64 (x86_64)
---
--- Host: localhost    Database: diagramagantt
--- ------------------------------------------------------
--- Server version	8.3.0
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `diagramagantt`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `diagramagantt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `diagramagantt`;
-
---
--- Table structure for table `ejercicios`
---
-
-DROP TABLE IF EXISTS `ejercicios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ejercicios` (
-  `idEJERCICIO` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `idLOGIN` int NOT NULL,
-  PRIMARY KEY (`idEJERCICIO`),
-  KEY `idLOGIN` (`idLOGIN`),
-  CONSTRAINT `ejercicios_ibfk_1` FOREIGN KEY (`idLOGIN`) REFERENCES `login` (`idLOGIN`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ejercicios`
---
-
-LOCK TABLES `ejercicios` WRITE;
-/*!40000 ALTER TABLE `ejercicios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ejercicios` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `login`
---
+create database diagramagantt;
+use diagramagantt;
 
 DROP TABLE IF EXISTS `login`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `login` (
   `idLOGIN` int NOT NULL AUTO_INCREMENT,
   `USERNAME` varchar(45) NOT NULL,
@@ -64,53 +11,50 @@ CREATE TABLE `login` (
   PRIMARY KEY (`idLOGIN`),
   UNIQUE KEY `USERNAME` (`USERNAME`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `login`
---
 
 LOCK TABLES `login` WRITE;
-/*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES (1,'admin','1234','administrador'),(2,'lalo','12345','usuario'),(3,'lalo2','12345','administrador');
-/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+INSERT INTO `login` VALUES (1,'admin','1234','administrador');
 UNLOCK TABLES;
 
---
--- Table structure for table `tareas`
---
 
-DROP TABLE IF EXISTS `tareas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tareas` (
-  `idTAREA` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `fecha_inicio` date NOT NULL,
-  `fecha_terminacion` date NOT NULL,
-  `idEJERCICIO` int NOT NULL,
-  PRIMARY KEY (`idTAREA`),
-  KEY `idEJERCICIO` (`idEJERCICIO`),
-  CONSTRAINT `tareas_ibfk_1` FOREIGN KEY (`idEJERCICIO`) REFERENCES `ejercicios` (`idEJERCICIO`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE ejercicios (
+  idEJERCICIO int NOT NULL AUTO_INCREMENT,
+  nombre varchar(100) NOT NULL,
+  idLOGIN int NOT NULL,
+  PRIMARY KEY (idEJERCICIO),
+  FOREIGN KEY (idLOGIN) REFERENCES login (idLOGIN) ON DELETE CASCADE
+);
 
---
--- Dumping data for table `tareas`
---
+CREATE TABLE tareas (
+  idTAREA int NOT NULL AUTO_INCREMENT,
+  nombre varchar(100) NOT NULL,
+  fecha_inicio date NOT NULL,
+  fecha_terminacion date NOT NULL,
+  idEJERCICIO int NOT NULL,
+  idDependencia int NULL,
+  PRIMARY KEY (idTAREA),
+  FOREIGN KEY (idEJERCICIO) REFERENCES ejercicios (idEJERCICIO) ON DELETE CASCADE,
+  FOREIGN KEY (idDependencia) REFERENCES tareas (idTAREA) ON DELETE SET NULL
+);
 
-LOCK TABLES `tareas` WRITE;
-/*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO ejercicios (nombre, idLOGIN) values ('Graficadora Diagramas de Gantt', 1);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, `idEJERCICIO`) values ('Obtencion de requerimientos', '2026-05-18', '2026-05-25', 1);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO, idDependencia) values ('Documentacion', '2026-05-26', '2026-05-29', 1, 1);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO, idDependencia) values ('Codificacion', '2026-06-01', '2026-06-19', 1, 2);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO) values ('Codificacion', '2026-06-20', '2026-06-21', 1);
 
--- Dump completed on 2026-06-17  0:13:52
+INSERT INTO ejercicios (nombre, idLOGIN) values ('Proyecto de Ejemplo 1', 1);
+
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO) values ('Tarea A', '2026-05-18', '2026-05-25', 1);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO, idDependencia) values ('Tarea B', '2026-05-26', '2026-05-29', 1, 5);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO, idDependencia) values ('Tarea C', '2026-06-01', '2026-06-19', 1, 6);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO, idDependencia) values ('Tarea D', '2026-06-20', '2026-06-21', 1, 7);
+
+
+INSERT INTO ejercicios (nombre, idLOGIN) values ('Proyecto de Ejemplo 2', 1);
+
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO) values ('Tarea A', '2026-05-18', '2026-05-25', 1);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO) values ('Tarea B', '2026-05-26', '2026-05-29', 1);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO) values ('Tarea C', '2026-06-01', '2026-06-19', 1);
+INSERT INTO tareas (nombre, fecha_inicio, fecha_terminacion, idEJERCICIO) values ('Tarea D', '2026-06-20', '2026-06-21', 1);
