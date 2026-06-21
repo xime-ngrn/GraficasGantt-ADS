0b8f2355d41bc654a71d6bdacab0f42d8063a31d
xime-ngrn <moreno.nogueron.ximena@gmail.com>
2026-06-21 00:57:52 -0600

    Cambio del puerto para el frontend

--- frontendbaseloginCRUD/webpack.config.js
+++ frontendbaseloginCRUD/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
       static: {
         directory: path.join(__dirname, 'dist'),
       },
-      port: 3000,
+      port: 8080,
       open: true,
       hot: true,
       historyApiFallback: true,
